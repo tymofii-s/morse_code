a79615b6edfabd647fee0a374df889aa82d03174
tymofii-s <t967887744@gmail.com>
2024-10-16 18:02:52 +0300

    version-8 upload

---
+++ translate.js
@@ -0,0 +1,267 @@
+const UArray = ["а", "б", "в", "г", "ґ", "д", "е", "є", "ж", "з", "и", "і",
+                "ї", "й", "к", "л", "м", "н", "о", "п", "р", "с", "т", "у",
+                 "ф", "х", "ц", "ч", "ш", "щ", "ь", "ю", "я",
+                 ".", ",", "!", "?", "<", ">", "/", ";", ":", "+", "-",
+                  "_", "*", "&", "^", "%", "$", "@"];
+
+const morseCode = [".-", "-...", ".--", "....", "---.", "-..", ".", "..-..",
+                  "...-", "--..", "-.--", "..", ".---.", ".---", "-.-", ".-..",
+                   "--", "-.", "---", ".--.", ".-.", "...", "-", "..-", "..-.",
+                    "----", "-.-.", "---.", "--.-", "--.--", "-..-", "..--", ".-.-",
+                    "·-·-·-",   // .
+                    "--··--",   // ,
+                    "-·-·--",   // !
+                    "··--··",   // ?
+                    "-···-",    // <
+                    "-·--·",    // >
+                    "-··-",     // /
+                    "-·-·-·",   // ;
+                    "---···",   // :
+                    "·-·-·",    // +
+                    "-····-",    // -
+                    "··--·-",   // _
+                    "-·-·-",    // *
+                    "·-···",    // &
+                    "···-·",    // ^
+                    "--··--",   // %
+                    "···-··-",  // $
+                    "·--·-·"    // @
+                ];
+
+const specialSymbols = ["[", "]", "{", "}", "(", ")", "=", "#"]
+
+
+
+function checkSpace(currentElement) {
+    if (currentElement === " ") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function checkUpSlash(currentElement) {
+    if (currentElement === "|") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function checkSpecialSymbols(currentElement) {
+    if (specialSymbols.includes(currentElement)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function symbolExist(currentElement) {
+    if (checkSpecialSymbols(currentElement) || morseCode.includes(currentElement) || UArray.includes(currentElement)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// function addP(symbolsThatNotExist) {
+//     deleteOldP()
+//     changeSectionHeight("unknownDiv1", symbolsThatNotExist)
+//     changeSectionHeight("section1", symbolsThatNotExist)
+
+//     for (let i = 0; i < symbolsThatNotExist.length; i++) {
+//         let section = document.getElementById('unknownDiv1')
+//         let newP = document.createElement("p")
+//         newP.textContent = symbolsThatNotExist[i];
+//         section.appendChild(newP);
+//     }
+// }
+
+// function deleteOldP() {
+//     let section = document.getElementById("unknownDiv1");
+//     let paragraphs = section.querySelectorAll('p');
+//     paragraphs.forEach(function(p) {
+//         p.parentNode.removeChild(p);
+//     });
+// }
+
+function addP(symbolsThatNotExist, sectionId1, sectionId2) {
+    deleteOldP(sectionId1)
+    changeSectionHeight(sectionId1, symbolsThatNotExist)
+    changeSectionHeight(sectionId2, symbolsThatNotExist)
+
+    for (let i = 0; i < symbolsThatNotExist.length; i++) {
+        let section = document.getElementById(sectionId1)
+        let newP = document.createElement("p")
+        newP.textContent = `${i+1}. ${symbolsThatNotExist[i]}`;
+        section.appendChild(newP);
+    }
+}
+
+function deleteOldP(sectionId) {
+    let section = document.getElementById(sectionId);
+    let paragraphs = section.querySelectorAll('p');
+    paragraphs.forEach(function(p) {
+        p.parentNode.removeChild(p);
+    });
+}
+
+function changeSectionHeight(sectionId, symbolsThatNotExist) {
+    let sectionToChange = document.getElementById(sectionId);
+
+    let unknownDivHeight = (symbolsThatNotExist.length) * 40 + 100;
+    let addSectionHeight = 450;
+    switch (sectionId) {
+        case "unknownDiv1":
+            sectionToChange.style.height = `${unknownDivHeight}px`; 
+            break;
+
+        case "section1":
+            if ((unknownDivHeight + addSectionHeight) >= 500) {
+                sectionToChange.style.height = `${unknownDivHeight + addSectionHeight}px`;
+                break
+            } else {
+                sectionToChange.style.height = "500px";
+                break
+            }
+
+        case "unknownDiv2":
+            sectionToChange.style.height = `${unknownDivHeight}px`; 
+            break;
+
+        case "section2":
+            if ((unknownDivHeight + addSectionHeight) >= 500) {
+                sectionToChange.style.height = `${unknownDivHeight + addSectionHeight}px`;
+                break
+            } else {
+                sectionToChange.style.height = "500px";
+                break
+            }
+    }
+}
+
+
+
+
+function deleteInputOfSection1() {
+    document.getElementById("ukr_morze").value = ""
+}
+
+function deleteUnknownListSection1() {
+    symbolsThatNotExist = [];
+    deleteOldP("unknownDiv1")
+    changeSectionHeight("unknownDiv1", symbolsThatNotExist)
+    changeSectionHeight("section1", symbolsThatNotExist)
+}
+
+function deleteInputOfSection2() {
+    document.getElementById("morze_ukr").value = ""
+}
+
+function deleteUnknownListSection2() {
+    symbolsThatNotExist = [];
+    deleteOldP("unknownDiv2")
+    changeSectionHeight("unknownDiv2", symbolsThatNotExist)
+    changeSectionHeight("section2", symbolsThatNotExist)
+}
+
+
+
+
+
+let symbolsThatNotExist = [];
+function ukrMorseTransleteFunction() {
+    let unformatedInput = document.getElementById("ukr_morze").value;
+    input = unformatedInput.toLowerCase();
+    let result = [];
+    symbolsThatNotExist = [];
+
+    for (let i = 0; i < input.length; i++) {
+        if (checkSpecialSymbols(input[i])) {
+            result.push(input[i])
+        } else if (checkSpace(input[i])) {
+            result.push("|")
+        } else if (!symbolExist(input[i])) {
+            symbolsThatNotExist.push(input[i])
+        } else {
+            let indexOfCurrentElement = UArray.indexOf(input[i])
+            result.push(morseCode[indexOfCurrentElement])
+        }
+        
+    }
+
+    result = result.join(" ")
+    document.getElementById("output1").innerHTML = result
+
+    if (symbolsThatNotExist.length !== 0) {
+        addP(symbolsThatNotExist, "unknownDiv1", "section1");
+    } else {
+        deleteOldP("unknownDiv1")
+        changeSectionHeight("unknownDiv1", symbolsThatNotExist)
+        changeSectionHeight("section1", symbolsThatNotExist)
+
+        let section = document.getElementById('unknownDiv1')
+        let newP = document.createElement("p")
+        newP.textContent = "Немає";
+        section.appendChild(newP);
+
+        let mainerSection = document.getElementById("section1")
+        section.style.height = "150px";
+        mainerSection.style.height = "600px"
+
+    }
+}
+
+function morseUkrTransleteFunction() {
+    let input = document.getElementById("morze_ukr").value;
+    input = input.split(" ")
+    let result = []
+    symbolsThatNotExist = [];
+
+    for (let i = 0; i < input.length; i++) {
+        if (checkSpecialSymbols(input[i])) {
+            result.push(input[i])
+        } else if (checkUpSlash(input[i])) {
+            result.push(" ")
+        } else if (!symbolExist(input[i])) {
+            symbolsThatNotExist.push(input[i])
+        } else {
+            let indexOfCurrentElement = morseCode.indexOf(input[i])
+            result.push(UArray[indexOfCurrentElement])
+        }
+    }
+
+    result = result.join("")
+    document.getElementById("output2").innerHTML = result
+
+    if (symbolsThatNotExist.length !== 0) {
+        addP(symbolsThatNotExist, "unknownDiv2", "section2");
+    } else {
+        deleteOldP("unknownDiv2")
+        changeSectionHeight("unknownDiv2", symbolsThatNotExist)
+        changeSectionHeight("section2", symbolsThatNotExist)
+
+        let section = document.getElementById('unknownDiv2')
+        let newP = document.createElement("p")
+        newP.textContent = "Немає";
+        section.appendChild(newP);
+
+        let mainerSection = document.getElementById("section2")
+        section.style.height = "150px";
+        mainerSection.style.height = "600px"
+    }
+}
+
+let responseList = []
+function getResponce() {
+    let currentResponse = document.getElementById("response").value;
+    let frase = document.getElementById("frase")
+
+    if (currentResponse === "") {
+        frase.innerHTML = "Введіть щось"
+    } else {
+        frase.innerHTML = "Ваш відгук відправлено!"
+        responseList.push(currentResponse)
+    }
+    document.getElementById("response").value = "";
+}
